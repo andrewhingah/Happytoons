356110217d9343d18f1112ab9832a0e41bc3dba9
andrewhingah <andrewhinga5@gmail.com>
2018-03-02 12:08:05 +0300

    update user.py

---
+++ user.py
@@ -0,0 +1,45 @@
+# print ("Happytoons")
+# def user_information:
+# 	information = {"name":"password"}
+# 	name = input ("enter your name")
+# 	password = input ("enter your password"
+# 	level = {:}
+# def reg (self, name,password, level):
+# 	name = input ("enter your name")
+# 	password = input ("enter your password")
+# 	level = input ("user type")
+# def level_type (reg):
+# 	if level = user:
+# 		print ("Click here to view your comments")
+# 		print ("Edit your comments")
+# 	elif level = moderator:
+# 		print ("Click here to view your comments")
+# 		print ("Edit your comments")
+# 		print ("Delete comments")
+# 	else:
+# 		print ("Select the comment you want to edit")
+# 		print ("Select the comment to delete")
+
+n = input ("enter you name:")
+user_info = {}
+#key = password
+#name = values
+for i in range (n):
+	password = input ("enter your password")
+	name = input ("enter your name")
+	user_info [password] = name
+
+level = " "
+
+level = input ("what's your level")
+if level = "admin":
+	print ("Select the comment you want to edit")
+	print ("Select the comment to delete")
+elif level = 'user':
+	print ("Click here to view your comments")
+	print ("Edit your comments")
+else:
+	print ("Click here to view your comments")
+	print ("Edit your comments")
+	print ("Delete comments")
+
