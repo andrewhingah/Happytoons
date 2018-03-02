bb93e02bef2a104a4cddba6cbbf626f369cc3fc8
andrewhingah <andrewhinga5@gmail.com>
2018-03-02 12:54:26 +0300

    Update to user types

--- user.py
+++ user.py
@@ -20,22 +20,20 @@
 # 		print ("Select the comment you want to edit")
 # 		print ("Select the comment to delete")
 
-n = input ("enter you name:")
-user_info = {}
+# n = input ("enter you name:")
+# user_info = {}
 #key = password
 #name = values
-for i in range (n):
-	password = input ("enter your password")
-	name = input ("enter your name")
-	user_info [password] = name
 
-level = " "
-
-level = input ("what's your level")
-if level = "admin":
+password = input ("enter your password")
+name = input ("enter your name")
+# info = user_info [password] = name
+if name == "Andrew":
+	print ("welcome admin")
 	print ("Select the comment you want to edit")
 	print ("Select the comment to delete")
-elif level = 'user':
+elif name == 'Liz':
+	print ("welcome user")
 	print ("Click here to view your comments")
 	print ("Edit your comments")
 else:
