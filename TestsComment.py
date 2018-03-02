dba7956b512150d318ce3130f2173d328fdc0f77
naibor <liznaibor.en@gmail.com>
2018-03-02 12:36:51 +0300

    update

--- TestsComment.py
+++ TestsComment.py
@@ -6,13 +6,20 @@ class TestComment(unittest.TestCase):
     def setUp(self):
         self.comment =UserComments("collins","my first comment")
 
+    def tearDown(self):
+        UserComments.comments_list=[]
+
     def test_initialize_comments(self):
         self.assertEqual(self.comment.author,"collins")
         self.assertEqual(self.comment.description,"my first comment")
         
-
     def test_save_comment(self):
         self.comment.saveComment()
         self.assertEqual(len(UserComments.comments_list),1)
+
+    # def test_update_comment(self):
+        # self.comment.editComment()
+        # self.comment.savecomment()
+        # self.assertEqual((UserComments.comments_list.description),"newcomment")
 
 
--- comments.py
+++ comments.py
@@ -7,3 +7,12 @@ class UserComments:
 # add comment
     def saveComment (self):
         UserComments.comments_list.append(self)
+
+    # def editComment (self,newcomment,oldcomment):
+    #     for comment in UserComments.comments_list:
+    #         if comment.description == oldcomment:
+    #             comment.description.replace(oldcomment,newcomment)
+    #             return newcomment
+
+
+    
